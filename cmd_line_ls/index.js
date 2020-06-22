b18fa9cb6e916c34e996049b6b1cff7545a68a69
Austin <65687945+austinhdang@users.noreply.github.com>
2020-06-22 19:05:13 -0400

    Convert tabs to spaces

--- cmd_line_ls/index.js
+++ cmd_line_ls/index.js
@@ -3,9 +3,9 @@
 const fs = require('fs');
 
 fs.readdir(process.cwd(), (err, filenames) => {
-	if (err) {
-		throw new Error(err);
-	}
+    if (err) {
+        throw new Error(err);
+    }
 
-	console.log(filenames);
+    console.log(filenames);
 });
